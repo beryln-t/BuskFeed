8a406abab0d960d655d832835c9d5bef2431fc89
beryln-t <berylntee@gmail.com>
2023-03-13 18:57:06 +0800

    create read delete for events

--- controllers/events.js
+++ controllers/events.js
@@ -1,14 +1,73 @@
 const Event = require("../models/event");
 
-const index = (req, res) => {
-  Event.find()
-    .exec()
-    .then((events) => {
-      console.log("events", events);
-      res.render("events/index", { events });
-    });
+const index = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const events = await Event.find().exec();
+    res.render("events/index", { events, id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal server error");
+  }
+};
+
+const myEvents = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const events = await Event.find().exec();
+    res.render("events/myevents", { events, id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal server error");
+  }
+};
+
+const show = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const event = await Event.findById(id).exec();
+    res.render("events/show", { event, id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal server error");
+  }
+};
+
+const newEvent = async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.render("events/new", { events });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal server error");
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const event = new Event(req.body).exec();
+    const e = await event.save();
+    res.redirect("/events");
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
+};
+
+const del = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const events = await Event.findByIdAndDelete(id).exec();
+    res.redirect("/events/myevents");
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
 };
 
 module.exports = {
   index,
+  myEvents,
+  show,
+  newEvent,
+  create,
+  del,
 };
--- routes/events.js
+++ routes/events.js
@@ -2,6 +2,13 @@ const express = require("express");
 const router = express.Router();
 const eventsCtrl = require("../controllers/events");
 
+router.get("/new", eventsCtrl.newEvent);
+router.post("/", eventsCtrl.create);
 router.get("/", eventsCtrl.index);
+router.get("/myevents", eventsCtrl.myEvents);
+router.delete("/:id", eventsCtrl.del);
+router.get("/:id", eventsCtrl.show);
+router.get("/:id", eventsCtrl.edit);
+router.put("/:id", eventsCtrl.update);
 
 module.exports = router;
